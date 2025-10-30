5bd27b2a4236543cf443293ac6626eff087620f7
LoganWin <logan9win@gmail.com>
2025-10-29 20:46:35 -0400

    finished problem 6

--- Problem6.py
+++ Problem6.py
@@ -6,7 +6,7 @@ Created on Tue Oct 28 11:35:48 2025
 @author: logan
 """
 
-str = "We tried list and we tried dicts also we tried Zen"
+str = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom let it be And in my hour of darkness she is standing right in front of me Speaking words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the broken hearted people living in the world agree There will be an answer let it be For though they may be parted there is still a chance that they will see There will be an answer let it be Let it be let it be let it be let it be There will be an answer let it be Let it be let it be let it be let it be Whisper words of wisdom let it be Let it be let it be let it be let it be Whisper words of wisdom let it be And when the night is cloudy there is still a light that shines on me Shine until tomorrow let it be I wake up to the sound of music Mother Mary comes to me Speaking words of wisdom let it be Let it be let it be let it be yeah let it be There will be an answer let it be Let it be let it be let it be yeah let it be Whisper words of wisdom let it be"
 dic = {}
 
 for word in str.split(' '):
